fe16db4025d4aa59dfc457144b45cf686803eda3
apemanjosh67 <76886612+apemanjosh67@users.noreply.github.com>
2022-12-25 16:39:47 -0500

    Added inverse trigonmetry
    Can now toggle between trig and inverse trig functions using the INV button
    Calculator will show whether it is in trig or inverse mode in the bottom right of the display panel

---
+++ functions.py
@@ -0,0 +1,254 @@
+import math
+
+def exponents(equation):
+    equation3 = equation
+
+    #remove unecessary operators
+    for i in range(len(equation3)):
+        if equation3[i] == '+' or equation3[i] == '-' or equation3[i] == '*' or equation3[i] == '/':
+            equation3 = equation3.replace(equation3[i], ' ', 1)
+    list = []
+    list = equation3.split()
+
+    #gather all expressions involving exponents ('^')
+    list2 = []
+    for i in list:
+        if '^' in i: list2.append(i)
+    #evaluate exponential expressions
+    list3 = list2[:]
+    answer = 1
+    for i in range(len(list3)):
+        list3[i] = list3[i].split('^')
+        for j in range(len(list3[i])-1, -1, -1):
+            answer = float(list3[i][j]) ** answer
+        #replace exponential expression with answer
+        equation = equation.replace(str(list2[i]), str(answer), 1)
+        answer = 1
+
+    return equation
+
+def sin(equation):
+    equation3 = equation
+
+    #remove unecessary operators
+    for i in range(len(equation3)):
+        if equation3[i] == '+' or equation3[i] == '*' or equation3[i] == '/':
+            equation3 = equation3.replace(equation3[i], ' ', 1)
+    for i in range(1, len(equation3)):
+        if equation[i] == '-':
+            if i >= 1:
+                if equation[i-1] != 'n':
+                    equation3 = equation3.replace(equation3[i], ' ', 1)
+
+    list = []
+    list = equation3.split()
+
+    list2 = []
+    for i in list:
+        if 'sin' in i: list2.append(i)
+
+    list3=list2[:]
+    for i in range(len(list3)):
+        list3[i] = list3[i].replace('sin', '')
+        answer = math.sin(float(list3[i]))
+        equation = equation.replace('sin'+list3[i], str(answer))
+    return equation
+
+def cos(equation):
+    equation3 = equation
+
+    #remove unecessary operators
+    for i in range(len(equation3)):
+        if equation3[i] == '+' or equation3[i] == '*' or equation3[i] == '/':
+            equation3 = equation3.replace(equation3[i], ' ', 1)
+    for i in range(1, len(equation3)):
+        if equation[i] == '-':
+            if i >= 1:
+                if equation[i-1] != 's':
+                    equation3 = equation3.replace(equation3[i], ' ', 1)
+
+    list = []
+    list = equation3.split()
+
+    list2 = []
+    for i in list:
+        if 'cos' in i: list2.append(i)
+
+    list3=list2[:]
+    for i in range(len(list3)):
+        list3[i] = list3[i].replace('cos', '')
+        answer = math.cos(float(list3[i]))
+        equation = equation.replace('cos'+list3[i], str(answer))
+    return equation
+
+def tan(equation):
+    equation3 = equation
+
+    #remove unecessary operators
+    for i in range(len(equation3)):
+        if equation3[i] == '+' or equation3[i] == '*' or equation3[i] == '/':
+            equation3 = equation3.replace(equation3[i], ' ', 1)
+    for i in range(1, len(equation3)):
+        if equation[i] == '-':
+            if i >= 1:
+                if equation[i-1] != 'n':
+                    equation3 = equation3.replace(equation3[i], ' ', 1)
+
+    list = []
+    list = equation3.split()
+
+    list2 = []
+    for i in list:
+        if 'tan' in i: list2.append(i)
+
+    list3=list2[:]
+    for i in range(len(list3)):
+        list3[i] = list3[i].replace('tan', '')
+        if float(list3[i])%(math.pi/2) == 0: return 'Undefined'
+        answer = math.tan(float(list3[i]))
+        equation = equation.replace('tan'+list3[i], str(answer))
+    return equation
+
+def asin(equation):
+    equation3 = equation
+
+    #remove unecessary operators
+    for i in range(len(equation3)):
+        if equation3[i] == '+' or equation3[i] == '*' or equation3[i] == '/':
+            equation3 = equation3.replace(equation3[i], ' ', 1)
+    for i in range(1, len(equation3)):
+        if equation[i] == '-':
+            if i >= 1:
+                if equation[i-1] != 'n':
+                    equation3 = equation3.replace(equation3[i], ' ', 1)
+
+    list = []
+    list = equation3.split()
+
+    list2 = []
+    for i in list:
+        if 'arcsin' in i: list2.append(i)
+
+    list3=list2[:]
+    for i in range(len(list3)):
+        list3[i] = list3[i].replace('arcsin', '')
+        if float(list3[i])%(math.pi/2) == 0: return 'Undefined'
+        answer = math.asin(float(list3[i]))
+        equation = equation.replace('arcsin'+list3[i], str(answer))
+    return equation
+
+
+def acos(equation):
+    equation3 = equation
+
+    #remove unecessary operators
+    for i in range(len(equation3)):
+        if equation3[i] == '+' or equation3[i] == '*' or equation3[i] == '/':
+            equation3 = equation3.replace(equation3[i], ' ', 1)
+    for i in range(1, len(equation3)):
+        if equation[i] == '-':
+            if i >= 1:
+                if equation[i-1] != 's':
+                    equation3 = equation3.replace(equation3[i], ' ', 1)
+
+    list = []
+    list = equation3.split()
+
+    list2 = []
+    for i in list:
+        if 'arccos' in i: list2.append(i)
+
+    list3=list2[:]
+    for i in range(len(list3)):
+        list3[i] = list3[i].replace('arccos', '')
+        if float(list3[i])%(math.pi/2) == 0: return 'Undefined'
+        answer = math.acos(float(list3[i]))
+        equation = equation.replace('arccos'+list3[i], str(answer))
+    return equation
+
+
+def atan(equation):
+    equation3 = equation
+
+    #remove unecessary operators
+    for i in range(len(equation3)):
+        if equation3[i] == '+' or equation3[i] == '*' or equation3[i] == '/':
+            equation3 = equation3.replace(equation3[i], ' ', 1)
+    for i in range(1, len(equation3)):
+        if equation[i] == '-':
+            if i >= 1:
+                if equation[i-1] != 'n':
+                    equation3 = equation3.replace(equation3[i], ' ', 1)
+
+    list = []
+    list = equation3.split()
+
+    list2 = []
+    for i in list:
+        if 'arctan' in i: list2.append(i)
+
+    list3=list2[:]
+    for i in range(len(list3)):
+        list3[i] = list3[i].replace('arctan', '')
+        if float(list3[i])%(math.pi/2) == 0: return 'Undefined'
+        answer = math.atan(float(list3[i]))
+        equation = equation.replace('arctan'+list3[i], str(answer))
+    return equation
+
+
+def sqrt(equation):
+    equation3 = equation
+
+    #remove unecessary operators
+    for i in range(len(equation3)):
+        if equation3[i] == '+' or equation3[i] == '*' or equation3[i] == '/':
+            equation3 = equation3.replace(equation3[i], ' ', 1)
+    for i in range(1, len(equation3)):
+        if equation[i] == '-':
+            if i >= 1:
+                if equation[i-1] != 't':
+                    equation3 = equation3.replace(equation3[i], ' ', 1)
+
+    list = []
+    list = equation3.split()
+
+    list2 = []
+    for i in list:
+        if 'sqrt' in i: list2.append(i)
+
+    list3=list2[:]
+    for i in range(len(list3)):
+        list3[i] = list3[i].replace('sqrt', '')
+        if float(list3[i]) < 0: return 'Undefined'
+        answer = math.sqrt(float(list3[i]))
+        equation = equation.replace('sqrt'+list3[i], str(answer))
+    return equation
+
+def ln(equation):
+    equation3 = equation
+
+    #remove unecessary operators
+    for i in range(len(equation3)):
+        if equation3[i] == '+' or equation3[i] == '*' or equation3[i] == '/':
+            equation3 = equation3.replace(equation3[i], ' ', 1)
+    for i in range(1, len(equation3)):
+        if equation[i] == '-':
+            if i >= 1:
+                if equation[i-1] != 'n':
+                    equation3 = equation3.replace(equation3[i], ' ', 1)
+
+    list = []
+    list = equation3.split()
+
+    list2 = []
+    for i in list:
+        if 'ln' in i: list2.append(i)
+
+    list3=list2[:]
+    for i in range(len(list3)):
+        list3[i] = list3[i].replace('ln', '')
+        if float(list3[i]) <= 0: return 'Undefined'
+        answer = math.log(float(list3[i]))
+        equation = equation.replace('ln'+list3[i], str(answer))
+    return equation
+
